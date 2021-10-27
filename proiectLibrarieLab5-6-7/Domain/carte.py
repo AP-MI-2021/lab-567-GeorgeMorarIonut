705ed7d1d9bf97e620b4529b8452b2051a69a0f2
George Mora <morargeorge_02@yahoo.com>
2021-10-27 18:55:19 +0300

    Lab 5

--- proiectLibrarieLab5-6-7/Domain/carte.py
+++ proiectLibrarieLab5-6-7/Domain/carte.py
@@ -8,65 +8,67 @@ def creeazaVanzareCarte(id, titlu, gen, pret, reducere):
     :param tipReducere: string
     :return:
     """
-    return {
-        "id": id,
-        "titlu": titlu,
-        "gen": gen,
-        "pret": pret,
-        "reducere": reducere
-    }
+    lista = []
+
+    lista.append(id)
+    lista.append(titlu)
+    lista.append(gen)
+    lista.append(pret)
+    lista.append(reducere)
+
+    return lista
 
 
-def getId(carte):
+def getId(lista):
     """
     Ia id-ul unei carti.
     :param carte: dictionar ce retine o carte
     :return: id-ul cartii
     """
-    return carte["id"]
+    return lista[0]
 
 
-def getTitlu(carte):
+def getTitlu(lista):
     """
     Ia titlul unei carti.
-    :param carte: dictionar ce retine o carte
+    :param lista: dictionar ce retine o carte
     :return: titlul cartii
     """
-    return carte["titlu"]
+    return lista[1]
 
 
-def getGen(carte):
+def getGen(lista):
     """
     Ia genul unei carti.
-    :param carte:  dictionar ce retine o carte
+    :param lista:  dictionar ce retine o carte
     :return:  genul cartii
     """
-    return carte["gen"]
+    return lista[2]
 
 
-def getPret(carte):
+def getPret(lista):
     """
     Ia pretul unei carti.
-    :param carte: dictionar ce retine o carte
+    :param lista: dictionar ce retine o carte
     :return: pretul cartii
     """
-    return carte["pret"]
+    return lista[3]
 
 
-def getReducere(carte):
+def getReducere(lista):
     """
     Ia reducerea unei carti.
-    :param carte: dictionar ce retine o carte
+    :param lista: dictionar ce retine o carte
     :return: reducerea clientului
     """
-    return carte["reducere"]
+    return lista[4]
 
 
-def toString(carte):
+def toString(lista):
     return "Id: {}, Titlu: {}, Gen: {}, Pret: {}, Reducere: {}".format(
-        getId(carte),
-        getTitlu(carte),
-        getGen(carte),
-        getPret(carte),
-        getReducere(carte)
+        lista[0],
+        lista[1],
+        lista[2],
+        lista[3],
+        lista[4]
     )
